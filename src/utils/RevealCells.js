13c6edc6082794c64ad37509ab39b23b451ef00b
TannerLankford1223 <T.Lankford730@gmail.com>
2022-06-05 05:39:34 -0500

    Add helper function that reveals surrounding cells

---
+++ src/utils/RevealCells.js
@@ -0,0 +1,96 @@
+function revealCells(arr, x, y, emptySpacesLeft) {
+
+    let cellsToReveal = [];
+    cellsToReveal.push(arr[x][y]);
+
+    while (cellsToReveal.length !== 0) {
+        let cell = cellsToReveal.pop();
+        let i = cell.x;
+        let j = cell.y;
+
+        if (!cell.fired) {
+            emptySpacesLeft--;
+            cell.fired = true;
+        }
+        if (cell.value !== 0) {
+            break;
+        }
+
+        if (i > 0 && j > 0 && arr[i - 1][j - 1].value === 0 && !arr[i - 1][j - 1].fired) {
+            cellsToReveal.push(arr[i - 1][j - 1]);
+        }
+
+        if (i < arr.length - 1 && j < arr[0].length - 1 && arr[i + 1][j + 1].value === 0 && !arr[i + 1][j + 1].fired) {
+            cellsToReveal.push(arr[i + 1][j + 1]);
+        }
+        if (i > 0 && j < arr[0].length - 1 && arr[i - 1][j + 1].value === 0 && !arr[i - 1][j + 1].fired) {
+            cellsToReveal.push(arr[i - 1][j + 1]);
+        }
+
+        if (i < arr.length - 1 && j > 0 && arr[i + 1][j - 1].value === 0 && !arr[i + 1][j - 1].fired) {
+            cellsToReveal.push(arr[i + 1][j - 1]);
+        }
+
+        if (i > 0 && arr[i - 1][j].value === 0 && !arr[i - 1][j].fired) {
+            cellsToReveal.push(arr[i - 1][j]);
+        }
+
+        if (j < arr[0].length - 1 && arr[i][j + 1].value === 0 && !arr[i][j + 1].fired) {
+            cellsToReveal.push(arr[i][j + 1]);
+        }
+
+        if (i < arr.length - 1 && arr[i + 1][j].value === 0 && !arr[i + 1][j].fired) {
+            cellsToReveal.push(arr[i + 1][j]);
+        }
+
+        if (j > 0 && arr[i][j - 1].value === 0 && !arr[i][j - 1].fired) {
+            cellsToReveal.push(arr[i][j - 1]);
+        }
+
+        // Reveal
+        if (i > 0 && j > 0 && !arr[i - 1][j - 1].fired) {
+            arr[i - 1][j - 1].fired = true;
+            emptySpacesLeft--;
+        }
+
+        if (j > 0 && !arr[i][j - 1].fired) {
+            arr[i][j - 1].fired = true;
+            emptySpacesLeft--;
+        }
+
+        if (i < arr.length - 1 && j > 0 && !arr[i + 1][j - 1].fired) {
+            arr[i + 1][j - 1].fired = true;
+            emptySpacesLeft--;
+        }
+
+        if (i > 0 && !arr[i - 1][j].fired) {
+            arr[i - 1][j].fired = true;
+            emptySpacesLeft--;
+        }
+
+        if (i < arr.length - 1 && !arr[i + 1][j].fired) {
+            arr[i + 1][j].fired = true;
+            emptySpacesLeft--;
+        }
+
+        if (i > 0 && j < arr[0].length - 1 && !arr[i - 1][j + 1].fired) {
+            arr[i - 1][j + 1].fired = true;
+            emptySpacesLeft--;
+        }
+
+        if (j < arr[0].length - 1 && !arr[i][j + 1].fired) {
+            arr[i][j + 1].fired = true;
+            emptySpacesLeft--;
+        }
+
+        if (i < arr.length - 1 && j < arr[0].length - 1 && !arr[i + 1][j + 1].fired) {
+            arr[i + 1][j + 1].fired = true;
+            emptySpacesLeft--;
+
+        }
+    }
+
+    return { arr, emptySpacesLeft };
+}
+
+export default revealCells;
